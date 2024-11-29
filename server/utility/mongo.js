7b3c74bc7a7b65c75a5016e18a136e8e54b0f227
atharav27 <110390023+atharav27@users.noreply.github.com>
2024-11-29 23:42:48 +0530

    updated imdex.js

--- server/utility/mongo.js
+++ server/utility/mongo.js
@@ -2,33 +2,25 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-let isConnected; // Variable to track connection status
+// let isConnected; // Variable to track connection status
 
+// Optimize database connection
 const connectToDatabase = async () => {
-  mongoose.set("strictQuery", true);
-  const url = process.env.MONGO_URI;
-  if (!url) throw new Error("MONGO_URI is not defined in .env file");
-
-  // If already connected, return the existing connection
-  if (isConnected) {
-    console.log("Already connected to the database");
-    return;
+  if (mongoose.connection.readyState === 1) {
+    return mongoose.connection;
   }
 
   try {
-    // Use the Mongoose connect method to connect to the database
-    await mongoose.connect(url, {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
-      // Remove poolSize if you receive an error
-      // Adjust serverSelectionTimeoutMS according to your needs
-      serverSelectionTimeoutMS: 5000,
+    return await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 3000,
+      socketTimeoutMS: 10000,
+      connectTimeoutMS: 5000,
+      maxPoolSize: 3,
+      bufferMaxEntries: 0,
     });
-    isConnected = true; // Update the connection status
-    console.log("Connected to Database");
   } catch (error) {
-    console.error("Database connection error:", error);
-    process.exit(1); // Exit the process on error
+    console.error("Database Connection Error:", error);
+    throw error;
   }
 };
 
