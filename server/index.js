2a823bb652eac1bd548362b0e3f17bfd1debb6ea
atharav27 <110390023+atharav27@users.noreply.github.com>
2024-11-29 21:42:24 +0530

    updated index.js

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ app.use("/api/referal", referalRouter);
 app.use("/api/admin", adminRouter);
 
 // Middleware for parsing JSON
-app.use(express.json());
+// app.use(express.json());
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
@@ -68,5 +68,5 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-export default app;
-export const handler = serverless(app);
+// export default app;
+export default serverless(app);
